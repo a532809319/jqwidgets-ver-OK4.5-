9fd83df862d0b27aa5956fe9178209927d68f860
hukai <532809319@qq.com>
2017-01-19 17:30:14 +0800

    it is    good

---
+++ demos/jqxgrid/aLocation.js
@@ -0,0 +1,54 @@
+var getLocalization = function (culture) {
+
+    localization =
+        {
+            // separator of parts of a date (e.g. '/' in 11/05/1955)
+            '/': "/",
+            // separator of parts of a time (e.g. ':' in 05:44 PM)
+            ':': ":",
+            // the first day of the week (0 = Sunday, 1 = Monday, etc)
+
+            percentsymbol: "%",
+            currencysymbol: "€",
+            currencysymbolposition: "after",
+            decimalseparator: '.',
+            thousandsseparator: ',',
+            pagergotopagestring: '[跳到]',
+            pagershowrowsstring: '[页码]',
+            sortascendingstring: "[正]",
+            sortdescendingstring: "Sort Descending",
+            // pagerrangestring: " von ",
+            // pagerpreviousbuttonstring: "nächster",
+            // pagernextbuttonstring: "voriger",
+            // pagerfirstbuttonstring: "first",
+            // pagerlastbuttonstring: "last",
+            // groupsheaderstring: "Ziehen Sie eine Kolumne und legen Sie es hier zu Gruppe nach dieser Kolumne",
+            // sortascendingstring: "Sortiere aufsteigend",
+            // sortdescendingstring: "Sortiere absteigend",
+            // sortremovestring: "Entferne Sortierung",
+            // groupbystring: "Group By this column",
+            // groupremovestring: "Remove from groups",
+            // filterclearstring: "Löschen",
+            // filterstring: "Filter",
+            // filtershowrowstring: "Zeige Zeilen, in denen:",
+            // filterorconditionstring: "Oder",
+            // filterandconditionstring: "Und",
+            // filterselectallstring: "(Alle auswählen)",
+            // filterchoosestring: "Bitte wählen Sie:",
+            // filterstringcomparisonoperators: ['leer', 'nicht leer', 'enthält', 'enthält(gu)',
+            //     'nicht enthalten', 'nicht enthalten(gu)', 'beginnt mit', 'beginnt mit(gu)',
+            //     'endet mit', 'endet mit(gu)', 'equal', 'gleich(gu)', 'null', 'nicht null'],
+            // filternumericcomparisonoperators: ['gleich', 'nicht gleich', 'weniger als', 'kleiner oder gleich', 'größer als', 'größer oder gleich', 'null', 'nicht null'],
+            // filterdatecomparisonoperators: ['gleich', 'nicht gleich', 'weniger als', 'kleiner oder gleich', 'größer als', 'größer oder gleich', 'null', 'nicht null'],
+            // filterbooleancomparisonoperators: ['gleich', 'nicht gleich'],
+            // validationstring: "Der eingegebene Wert ist ungültig",
+            // emptydatastring: "Nokeine Daten angezeigt",
+            // filterselectstring: "Wählen Sie Filter",
+            // loadtext: "Loading...",
+            // clearstring: "Löschen",
+            // todaystring: "Heute"
+        }
+
+
+    return localization;
+}
